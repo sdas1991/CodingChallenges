1bc284f45cab23dfc5338958fdb8514cec3a0495
sdas1991 <sourangshu_das_tict@yahoo.co.in>
2020-07-13 21:35:16 -0700

    two sum

--- algorithm-challenges-solutions/src/com/algo/google/challenges/TwoSum.java
+++ algorithm-challenges-solutions/src/com/algo/google/challenges/TwoSum.java
@@ -1,10 +1,34 @@
 package com.algo.google.challenges;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+	
+	public static int[] twoSum(int[] input, int target) {
+		int[] output = new int[2];		
+		Map<Integer, Integer> exists=new HashMap<>();
+		for (int i = 0; i < input.length; i++) {
+			if(exists.containsKey(target-input[i])) {
+				output[0]=exists.get(target-input[i]);
+				output[1]=i;
+				return output;
+			}
+			exists.put(input[i], i);
+		}	
+		
+		return output;
+	}
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[] input = {2,5,8,4,6,3,0};
+		int target = 9;
+		int[] output = new int[2];
+		output=twoSum(input, target);
+		System.out.println(output[0]+" "+output[1]);
 	}
 
 }
